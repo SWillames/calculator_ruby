bff887e6fb0b3d282d8457515c03da77e29171c5
Sergio <tp.sergio.willames@totvspartner.com.br>
2023-10-04 02:30:11 -0300

    writing the tests for the other operations.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -11,5 +11,17 @@ describe Calculator do
       sum = calculator.add(8, 23)
       expect(sum).to eql(31)
     end
+
+    it "can subtract the numbers using the subtract method" do
+      expect(@calc.subtract(6,4)).to eql(2)
+    end
+
+    it "can multiply the numbers using the multiply method" do
+      expect(@calc.multiply(3,4)).to eql(12)
+    end
+
+    it "can divide the numbers using the divide method" do
+      expect(@calc.divide(18,3)).to eql(6)
+    end
   end
 end
