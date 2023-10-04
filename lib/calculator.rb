3f964da6fb3fdd17e7be435ecb764f955a95fc5f
Sergio <tp.sergio.willames@totvspartner.com.br>
2023-10-04 04:07:06 -0300

    making all tests pass.

---
+++ lib/calculator.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Calculator
+
+  def add(x, y)
+    return x + y
+  end
+
+  def subtract(x, y)
+    return x - y
+  end
+
+  def multiply(x, y)
+    return x*y
+  end
+
+  def divide(x, y)
+    return x / y
+  end
+end
