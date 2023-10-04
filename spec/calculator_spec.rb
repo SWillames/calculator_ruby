5f02a2a60aa7648902ba372016fc9356eadd78db
Sergio <tp.sergio.willames@totvspartner.com.br>
2023-10-04 02:47:53 -0300

    refactoring the code.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,27 +1,30 @@
 describe Calculator do
+
+  before(:each) do
+    @calculator = Calculator.new
+  end
+
   context "Given two numbers" do
     it "can add the numbers using the add method" do
-      calculator = Calculator.new
-      sum = calculator.add(2, 3)
+      sum = @calculator.add(2, 3)
       expect(sum).to eql(5)
     end
 
     it "can add two numbers" do
-      calculator  = Calculator.new
-      sum = calculator.add(8, 23)
+      sum = @calculator.add(8, 23)
       expect(sum).to eql(31)
     end
 
     it "can subtract the numbers using the subtract method" do
-      expect(@calc.subtract(6,4)).to eql(2)
+      expect(@calculator.subtract(6,4)).to eql(2)
     end
 
     it "can multiply the numbers using the multiply method" do
-      expect(@calc.multiply(3,4)).to eql(12)
+      expect(@calculator.multiply(3,4)).to eql(12)
     end
 
     it "can divide the numbers using the divide method" do
-      expect(@calc.divide(18,3)).to eql(6)
+      expect(@calculator.divide(18,3)).to eql(6)
     end
   end
 end
